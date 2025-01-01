d6f174765325010805af06c614a3d0646c5c15de
Ravindu Pasanjith <IT22898920@my.sliit.lk>
2025-01-01 20:21:34 +0530

    redux - register backend

--- frontend/src/pages/auth/Register.js
+++ frontend/src/pages/auth/Register.js
@@ -1,10 +1,88 @@
-import React from 'react'
+import { useState } from "react";
 import styles from "./auth.module.scss";
+import registerImg from "../../assets/register.png";
+import Card from "../../components/card/Card";
+import { Link } from "react-router-dom";
+import Loader from "../../components/loader/Loader";
 
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  cPassword: "",
+};
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [formData, setFormData] = useState(initialState);
+  const { name, email, password, cPassword } = formData;
 
-export default Register
+      const registerUser = async (e) => {
+            e.preventDefault();
+
+      }
+
+        const handleInputChange = (e) => {
+          const { name, value } = e.target;
+          setFormData({ ...formData, [name]: value });
+        };
+
+  return (
+    <>
+      {/* {isLoading && <Loader />} */}
+      <section className={`container ${styles.auth}`}>
+        <Card>
+          <div className={styles.form}>
+            <h2>Register</h2>
+
+            <form onSubmit={registerUser}>
+              <input
+                type="text"
+                placeholder="Name"
+                required
+                name="name"
+                value={name}
+                onChange={handleInputChange}
+              />
+              <input
+                type="text"
+                placeholder="Email"
+                required
+                name="email"
+                value={email}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                name="password"
+                value={password}
+                onChange={handleInputChange}
+              />
+              <input
+                type="password"
+                placeholder="Confirm Password"
+                required
+                name="cPassword"
+                value={cPassword}
+                onChange={handleInputChange}
+              />
+              <button type="submit" className="--btn --btn-primary --btn-block">
+                Register
+              </button>
+            </form>
+
+            <span className={styles.register}>
+              <p>Already an account?</p>
+              <Link to="/login">Login</Link>
+            </span>
+          </div>
+        </Card>
+        <div className={styles.img}>
+          <img src={registerImg} alt="Register" width="400" />
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default Register;
